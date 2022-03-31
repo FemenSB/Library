c7480b6f4bad70210103cb2e4ead21b6d1e87b52
FemenSB <felipe_sborsato@hotmail.com>
2022-03-31 06:07:51 -0300

    POST data validation
    No books can be added with blank information

--- src/controllers/books_controller.js
+++ src/controllers/books_controller.js
@@ -1,11 +1,24 @@
 const booksModel = require('../models/books_model');
 
+function checkFields(obj, fields) { // Checks if the given object has the specified fields as properties
+  for(let i = 0; i < fields.length; i++) {
+    if(!obj[fields[i]]) {return false;}
+  }
+  return true;
+}
+
 function httpPostBook(req, res) {
+
+  if(!checkFields(req.body, ['title', 'author'])) {
+    res.status(400).send({error: 'Data missing!'});
+    return;
+  }
+
   let newBook = {
     title: req.body.title,
     author: req.body.author
   };
-  res.send(booksModel.postBook(newBook));
+  res.status(201).send(booksModel.postBook(newBook));
 }
 
 function httpPutBook(req, res) {
@@ -19,7 +32,7 @@ function httpPutBook(req, res) {
     return;
   }
 
-  res.status(404).send('Id not found!'); // If execution didn't stop, the book wasn't found
+  res.status(404).send({error: 'Id not found!'}); // If execution didn't stop, the book wasn't found
 
 }
 
@@ -29,7 +42,7 @@ function httpDeleteBook(req, res) {
     res.status(200).send('Book deleted!');
     return;
   }
-  res.status(404).send('Id not found!'); // If function execution didn't stop, the book was not found
+  res.status(404).send({error: 'Id not found!'}); // If function execution didn't stop, the book was not found
 }
 
 function httpGetAllBooks(req, res) { // No id specified
@@ -39,11 +52,12 @@ function httpGetAllBooks(req, res) { // No id specified
 function httpGetBook(req, res) { // Specific id requested
   let requestedId = parseInt(req.params.id); // Parse the requested id
   let bookFound = booksModel.getBook(requestedId); // Ask the model for this id
-  if(typeof bookFound !== undefined) { // The model will return undefined if the book wasn't found
+  if(bookFound !== undefined) { // The model will return undefined if the book wasn't found
     res.send(bookFound);
+    console.log('Book not undefined');
     return;
   }
-  res.status(404).send('Id not found!'); // If function execution didn't stop, the book was not found
+  res.status(404).send({error: 'Id not found!'}); // If function execution didn't stop, the book was not found
 }
 
 module.exports = {
